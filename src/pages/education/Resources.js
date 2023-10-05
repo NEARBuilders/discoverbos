f6c645ccc403cd668fb7d808ee3681b07368ee4b
Zeeshan Ahmad <itexpert120@outlook.com>
2023-10-05 17:54:17 +0500

    Add resouces page

---
+++ src/pages/education/Resources.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EducationTutorials from "./tutorials";
+import EducationWorkshops from "./Workshops";
+
+export default function Resources() {
+  return (
+    <div className="mb-3">
+      <EducationTutorials className="mb-3" />
+      <EducationWorkshops className="mb-3" />
+    </div>
+  );
+}
