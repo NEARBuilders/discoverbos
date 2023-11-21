74c8ec8acab96df1a6c817d6ba8c051e982b5d3d
Zeeshan Ahmad <itexpert120@outlook.com>
2023-11-21 12:48:44 +0500

    Update Code Reviews & Funding Opportunities - Resolves https://github.com/NEARBuilders/discoverbos/issues/51

--- src/pages/education/CodeReviews.js
+++ src/pages/education/CodeReviews.js
@@ -7,6 +7,27 @@ const links = [
     name: "BOS Code review: Building a Near Social bot and NFT barter application",
     href: "https://youtu.be/8-Y-eBxG_Cs?si=LlBGsWQ4G5G1T0zX",
   },
+  {
+    name: "Build A #BOS Bot & NFT Barter Application -",
+    href: "https://youtu.be/8-Y-eBxG_Cs?si=J_zzE_zGUYGbxX95",
+  },
+  {
+    name: "Creating SDKs on BOS (Lens, GenaDrop) + New Vue Inspired Skip Framework with Matt B",
+    href: "https://youtu.be/IVQoocmWDGU?si=J6sQWi3vZgAO70qF",
+  },
+  {
+    name: "Using SocialDB on #BOS to Create + Visualize Groups & Create Pull Requests",
+    href: "https://youtu.be/HAyweTF7IjU?si=zbs4m3qlGqzCOWip",
+  },
+  {
+    name: "Adding External Packages to the Your Gateway / #BOS VM",
+    href: "https://youtu.be/cJuHp8exNVY?si=weF9Sjt99U80yJZj",
+  },
+  {
+    name: "Build A Multichain #NFT Minter on #BOS",
+    href: "https://youtu.be/YJbnbfY7xOE?si=AsiiLb3cZ-VZDTYY",
+  },
+
 ];
 
 export default function EducationCodeReviews() {
